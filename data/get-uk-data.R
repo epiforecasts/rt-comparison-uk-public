50953f46ab01c1191923d24c8528cea0534e4993
kathsherratt <katharine.sherratt@lshtm.ac.uk>
2020-08-21 09:51:03 +0100

    deaths data
    deaths data available:
    - by date of death for
      - english regions
      - england, scotland, wales
    - by date of report for
      - england, scotland, wales, northern ireland

--- data/get-uk-data.R
+++ data/get-uk-data.R
@@ -5,21 +5,21 @@ library(ukcovid19) # remotes::install_github("publichealthengland/coronavirus-da
 
 # Set up query
 structure <- list("date", "areaName", 
-                  "newDeaths28DaysByDeathDate", "newCasesBySpecimenDate", 
-                  "newCasesByPublishDate", "newAdmissions")
+                  "newDeaths28DaysByDeathDate",  "newDeaths28DaysByPublishDate",
+                  "newCasesBySpecimenDate", "newCasesByPublishDate", "newAdmissions")
 names(structure) <- structure
 areaType <- list("region" = "areaType=region",
                  "nation" = "areaType=nation")
 
 # Get data
-data <- purrr::map_dfr(areaType, ~ ukcovid19::get_data(filters = .x, structure = structure))
+data <- purrr::map_dfr(areaType, ~ ukcovid19::get_data(filters = .x, structure = structure)) 
 
 # # Clean
 
 # Rename
 data.table::setDT(data)
 old <- unlist(structure)
-new <- c("date", "region", "deaths", "cases_test", "cases_publish", "cases_hosp")
+new <- c("date", "region", "deaths_death", "deaths_publish",  "cases_test", "cases_publish", "cases_hosp")
 data <- data.table::setnames(data, old, new)
 data$date <- as.Date(data$date)
 
@@ -28,8 +28,16 @@ nations <- c("England", "Scotland", "Wales", "Northern Ireland")
 regions <- setdiff(unique(data$region), nations)
 data$region_type <- ifelse(data$region %in% nations, "nation", "region")
 
-# cases_publish - regions show 0s where there should be NAs for cases by publish date
+# Add blended cases - publish date for nations, specimen date for regions
+data$cases_blend = ifelse(data$region %in% nations, data$cases_publish, data$cases_test)
+
+# Add blended deaths - publish date for nations, date of death for regions
+data$deaths_blend = ifelse(data$region %in% nations, data$deaths_publish, data$deaths_death)
+
+# Regions show 0s where there should be NAs for data by publish date
 data$cases_publish <- ifelse(data$region %in% regions, NA, data$cases_publish)
+data$deaths_publish <- ifelse(data$region %in% regions, NA, data$deaths_publish)
+
 
 # Check date sequence is complete
 if(length(seq.Date(from = min(data$date), to = max(data$date), by = 1)) 
--- data/plot-data.R
+++ data/plot-data.R
@@ -15,16 +15,16 @@ standardised_data <- data %>%
   # Standardise by region
   dplyr::group_by(region) %>%
   #  - z-scores
-  dplyr::mutate(zcases_test = scale(cases_test, center = TRUE, scale = TRUE), 
-                zcases_publish = scale(cases_publish, center = TRUE, scale = TRUE), 
+  dplyr::mutate(zcases_blend = scale(cases_blend, center = TRUE, scale = TRUE), 
                 zcases_hosp = scale(cases_hosp, center = TRUE, scale = TRUE), 
-                zdeaths = scale(deaths, center = TRUE, scale = TRUE),
+                zdeaths_blend = scale(deaths_blend, center = TRUE, scale = TRUE),
    #  - 7-day moving average
-                ma_cases_test = forecast::ma(cases_test, order = 7), 
-                ma_cases_publish = forecast::ma(cases_publish, order = 7),
+                ma_cases_blend = forecast::ma(cases_blend, order = 7), 
                 ma_cases_hosp = forecast::ma(cases_hosp, order = 7), 
-                ma_deaths = forecast::ma(deaths, order = 7)) %>% 
-  dplyr::ungroup()
+                ma_deaths_blend = forecast::ma(deaths_blend, order = 7)) %>% 
+  dplyr::ungroup() %>%
+  dplyr::mutate(region = factor(region, levels = c("England", "Scotland", "Wales", "Northern Ireland",
+                                                   regions)))
 
 # Plot-ready z-scores ----------------------------------------------------------------
 data_zcore <- standardised_data %>%
@@ -32,11 +32,9 @@ data_zcore <- standardised_data %>%
   tidyr::pivot_longer(cols = dplyr::starts_with("z"), names_to = "variable", values_to = "zscore") %>%
   dplyr::mutate(variable = stringr::str_remove_all(variable, "^z")) %>%
   dplyr::mutate('Data source' = dplyr::recode_factor(variable, 
-                                                     "cases_test" = "Cases by test date",
-                                                     "cases_publish" = "Cases by report date",
+                                                     "cases_blend" = "Cases",
                                                      "cases_hosp" = "Hospital admissions",
-                                                     "deaths" = "Deaths")) %>%
-  dplyr::filter(region_type == "nation")
+                                                     "deaths_blend" = "Deaths"))
 
 # Plot-ready 7-day MA ----------------------------------------------------------------
 data_ma <- standardised_data %>%
@@ -44,17 +42,15 @@ data_ma <- standardised_data %>%
   tidyr::pivot_longer(cols = dplyr::starts_with("ma"), names_to = "variable", values_to = "ma") %>%
   dplyr::mutate(variable = stringr::str_remove_all(variable, "^ma_")) %>%
   dplyr::mutate('Data source' = dplyr::recode_factor(variable, 
-                                                     "cases_test" = "Cases by test date",
-                                                     "cases_publish" = "Cases by report date",
+                                                     "cases_blend" = "Cases",
                                                      "cases_hosp" = "Hospital admissions",
-                                                     "deaths" = "Deaths")) %>%
-  dplyr::filter(region_type == "nation")
+                                                     "deaths_blend" = "Deaths"))
   
 
 # National -------------------------------------------------------------
 
 # Plot 7 day MA
-plot_national_ma <- data_ma %>%
+plot_ma <- data_ma %>%
   ggplot() +
   geom_line(aes(x = date, y = as.numeric(ma), colour = `Data source`)) +
   facet_wrap(~ region, nrow = 1, scales = "free_y") +
@@ -65,18 +61,16 @@ plot_national_ma <- data_ma %>%
   #theme(axis.text.x = element_blank()) +
   labs(title = "Smoothed counts", y = "Count, 7-day moving average", x = "")
 
-# plot(plot_count_national)
-
 # Plot zscore
-# plot_national_zscore <- data_zscore %>%
-#   ggplot() +
-#   geom_line(aes(x = date, y = as.numeric(zscore), colour = `Data source`)) +
-#   #geom_hline(yintercept = 0, linetype = 2) +
-#   facet_wrap(~ region, nrow = 1, scales = "free_y") +
-#   cowplot::theme_cowplot(font_size = 11) +
-#   #scale_color_manual(values = colours) +
-#   theme(panel.spacing.x = unit(0.5, "cm")) +
-#   #theme(legend.position = "none") +
-#   #theme(axis.text.x = element_blank()) +
-#   labs(title = "Standardised raw counts", y = "z-score", x = "")
+plot_zscore <- data_zscore %>%
+  ggplot() +
+  geom_line(aes(x = date, y = as.numeric(zscore), colour = `Data source`)) +
+  #geom_hline(yintercept = 0, linetype = 2) +
+  facet_wrap(~ region, nrow = 1, scales = "free_y") +
+  cowplot::theme_cowplot(font_size = 11) +
+  #scale_color_manual(values = colours) +
+  theme(panel.spacing.x = unit(0.5, "cm")) +
+  #theme(legend.position = "none") +
+  #theme(axis.text.x = element_blank()) +
+  labs(title = "Standardised raw counts", y = "z-score", x = "")
 
