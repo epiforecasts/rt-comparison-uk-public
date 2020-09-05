c422412a6d29c27fa9f580d8b115cd27ab5cec87
Kath Sherratt <kathsherratt@users.noreply.github.com>
2020-09-05 18:16:02 +0100

    england case rates by age & data source

--- data/admissions-by-age.R
+++ data/admissions-by-age.R
@@ -4,22 +4,23 @@ structure <- list("date", "areaName",
                   "cumAdmissionsByAge")
 names(structure) <- structure
 areaType <- list("nhsregion" = "areaType=nhsregion")
+
 # Get data
 raw_admissions <- ukcovid19::get_data(filters = areaType, structure = structure) 
 
 
 # Clean -------------------------------------------------------------------
 age_admissions <- tidyr::unnest(raw_admissions, cumAdmissionsByAge) %>%
-  # dplyr::filter(areaName == "London") %>%
   dplyr::mutate(date = lubridate::ymd(date)) %>%
   # De-cumulate
   dplyr::group_by(areaName, age) %>%
   dplyr::arrange(date, .by_group = TRUE) %>%
-  dplyr::mutate(value = value - lag(value, 1)) %>%
+  dplyr::mutate(value = value - dplyr::lag(value, 1),
+                rate = rate - dplyr::lag(rate, 1)) %>%
   dplyr::ungroup() %>%
   # Pivot wide
-  dplyr::select(-rate) %>%
-  tidyr::pivot_wider(id_cols = c("date", "areaName"), names_from = "age", values_from = "value") %>%
+  dplyr::select(-value) %>%
+  tidyr::pivot_wider(id_cols = c("date", "areaName"), names_from = "age", values_from = "rate") %>%
   dplyr::rename(region = "areaName", 
                 age1864 = "18_to_64", 
                 age6584 = "65_to_84",
@@ -27,21 +28,25 @@ age_admissions <- tidyr::unnest(raw_admissions, cumAdmissionsByAge) %>%
                 age617 = "6_to_17",
                 age85 = "85+") %>%
   dplyr::group_by(region) %>%
-  dplyr::mutate(perc_u65 = (age05 + age617 + age1864) / (age1864 + age6584 + age05 + age617 + age85),
-                z_perc_u65 = scale(perc_u65, center = TRUE, scale = TRUE),
-                ma_perc_u65 = forecast::ma(perc_u65, order = 7)) %>%
+  dplyr::mutate(young_old = age1864 / (age6584+age85)) %>%
   dplyr::ungroup()
+
+  # dplyr::mutate(perc_u65 = (age05 + age617 + age1864) / (age1864 + age6584 + age05 + age617 + age85),
+  #               z_perc_u65 = scale(perc_u65, center = TRUE, scale = TRUE),
+  #               ma_perc_u65 = forecast::ma(perc_u65, order = 7)) %>%
+  # dplyr::ungroup()
 
 
 # Plot --------------------------------------------------------------------
+library(ggplot2)
 plot_age <- age_admissions %>%
   ggplot(aes(x = date, col = region, fill = region)) +
-  geom_line(aes(y = z_perc_u65)) +
+  geom_line(aes(y = age1864)) +
   cowplot::theme_cowplot(font_size = 11) +
   # scale_color_manual(values = colours) +
   theme(panel.spacing.x = unit(0.5, "cm")) +
   # theme(legend.position = "none") +
   # theme(axis.text.x = element_blank()) +
-  labs(title = "Hospital admissions, % under 65yrs", y = "% admissions under 65", x = "")
+  labs(title = "Hospital admissions among 18-65yrs", y = "Rate per 100,000", x = "")
 
 plot(plot_age)
---
+++ data/age-settings-data.R
@@ -0,0 +1,108 @@
+# Showing a moving epidemic between sub-populations:
+# - Age distribution of cases
+# - Outbreak settings (care homes, workplaces)
+
+# Data source: national surveillance reports, Public Health England
+# URL: https://www.gov.uk/government/publications/national-covid-19-surveillance-reports
+
+library(magrittr); library(ggplot2)
+
+xls_url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/914814/Weekly_COVID19_report_data_w36.xlsx"
+
+httr::GET(xls_url, httr::write_disk(tf <- tempfile(fileext = ".xlsx")))
+
+sheets <- readxl::excel_sheets(tf)
+
+# Set up dates from weeks
+dates <- tibble::tibble(week = c(35:5),
+                        date = seq.Date(from = as.Date("2020-08-30"), by = -7, length.out = length(c(5:35))))
+
+
+# By setting --------------------------------------------------------------
+
+#--- Number of acute respiratory infection incidents by institution
+
+setting <- readxl::read_excel(path = tf,
+                      sheet = grep("Outbreaks", sheets),
+                      range = readxl::cell_limits(c(9, 2), c(NA, NA))) %>%
+  dplyr::rename(week = ...1)
+
+# Trim to weeks in other covid datasets (remove weeks from last year)
+setting <- setting[18:nrow(setting),]
+
+
+
+# By age ------------------------------------------------------------------
+# #--- Weekly incidence per 100,000 population by age group by region, weeks 31-35
+# age_region <- readxl::read_excel(path = tf,
+#                                  sheet = grep("Incidence-age region", sheets),
+#                                  range = readxl::cell_limits(c(9, 2), c(NA, NA)))
+# colnames(age_region) <- c("week", stringr::str_c("age_", stringr::str_replace_all(colnames(age_region[2:length(age_region)]), "-", "_")))
+# age_region <- age_region %>%
+#   # Clean to get region names
+#   dplyr::mutate(region = stringr::str_remove_all(week, "[[:digit:]]"),
+#                 region = ifelse(region == "", NA, region),
+#                 week = suppressWarnings(as.numeric(week))) %>%
+#   tidyr::fill(region, .direction = "down") %>%
+#   tidyr::drop_na()
+
+#--- Weekly lab confirmed cases per 100,000 population, Pillar 1 & 2, by age group, national, weeks 5-35
+age_nation <- readxl::read_excel(path = tf,
+                                 sheet = grep("Case rates by agegrp", sheets),
+                                 range = readxl::cell_limits(c(9, 2), c(NA, NA)))
+colnames(age_nation) <- c("week", stringr::str_c("age_", stringr::str_replace_all(colnames(age_nation[2:length(age_nation)]), "-", "_")))
+
+# Set numeric
+age_nation[,1:8] <- sapply(age_nation[,1:8], as.numeric)
+
+# Join under <64 and >65 ages
+age_nation$`<64 yrs` <- rowSums(age_nation[2:5])
+age_nation$`65+ yrs` <- rowSums(age_nation[6:8])
+age_nation <- age_nation[,c(1,9:10)]
+
+#--- Weekly hospital admission rate per 100,000 positive tests reported through CHESS, by age group
+age_admissions_chess <- readxl::read_excel(path = tf,
+                              sheet = grep("CHESS - age group", sheets),
+                              range = readxl::cell_limits(c(9, 2), c(NA, NA)))
+colnames(age_admissions_chess) <- c("week", stringr::str_c("age_", stringr::str_replace_all(colnames(age_admissions_chess[2:length(age_admissions_chess)]), "-", "_")))
+
+# Drop second table (ICU admission rates)
+age_admissions_chess <- age_admissions_chess[1:(grep("ICU", age_admissions_chess$age_0_4)-2),]
+
+# Set numeric
+age_admissions_chess[,1:8] <- sapply(age_admissions_chess[,1:8], as.numeric)
+
+# Join under <64 and >65 ages
+age_admissions_chess$`<64 yrs` <- rowSums(age_admissions_chess[2:5])
+age_admissions_chess$`65+ yrs` <- rowSums(age_admissions_chess[6:8])
+age_admissions_chess <- age_admissions_chess[,c(1,9:10)]
+
+#--- Join hospital admission rates and positive test rates
+age_rates <- dplyr::filter(age_nation, week >= 12) %>%
+  dplyr::bind_rows(age_admissions_chess, .id = "Data source") %>%
+  dplyr::mutate(`Data source` = ifelse(`Data source` == 1, "Community", "Hospital admissions")) %>%
+  tidyr::pivot_longer(cols = -c(week, `Data source`), names_to = "Age") %>%
+  dplyr::left_join(dates, by = "week")
+
+# Plot --------------------------------------------------------------------
+
+plot_age <- age_rates %>%
+  ggplot(aes(x = date, group = interaction(Age, `Data source`), colour = Age)) +
+  geom_line(aes(y = value, linetype = `Data source`)) +
+  theme_classic() +
+  scale_colour_brewer(palette = "Dark2") +
+  theme(legend.position = c(0.85, 0.75)) +
+  labs(y = "Weekly confirmed cases per 100,000 population",
+       x = "",
+       title = "Rate of reported Covid-19 per 100,000 in England, 
+       by source of data and age")
+
+ggsave("figures/rate_by_age_source.png", plot_age, width = 5, height = 4)
+
+
+
+
+
+
+
+
