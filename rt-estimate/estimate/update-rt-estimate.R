9a889a21a03f337cc7705db89411673b919665cf
kathsherratt <kathsherratt@gmail.com>
2020-09-24 17:03:22 +0000

    death rt estimates with cocin delay

--- rt-estimate/estimate/update-rt-estimate.R
+++ rt-estimate/estimate/update-rt-estimate.R
@@ -21,15 +21,15 @@ data <- data[data$date >= as.Date("2020-03-19") & data$date <= Sys.Date()-2,]
 # Run everything everywhere
 # 
 # Cases - by test date
-run_rt_estimate(data = data,
-                count_variable = c("cases_test"),
-                reporting_delay = cases_delay,
-                burn_in = 0)
-# Hospital admissions
-run_rt_estimate(data = data,
-                count_variable = c("cases_hosp"), 
-                reporting_delay = cases_delay,
-                burn_in = 0)
+# run_rt_estimate(data = data,
+#                 count_variable = c("cases_test"),
+#                 reporting_delay = cases_delay,
+#                 burn_in = 0)
+# # Hospital admissions
+# run_rt_estimate(data = data,
+#                 count_variable = c("cases_hosp"), 
+#                 reporting_delay = cases_delay,
+#                 burn_in = 0)
 # Deaths - by date of death
 run_rt_estimate(data = data,
                 count_variable = c("deaths_death"),
